d591263e5973222799ea46ed47a8e731157f2a52
maoyifan <maoyifan_ee@163.com>
2022-08-18 18:31:56 +0800

    give up gesture detection

--- app/src/main/java/com/example/game2048/ClassicGameFragment.kt
+++ app/src/main/java/com/example/game2048/ClassicGameFragment.kt
@@ -2,13 +2,13 @@ package com.example.game2048
 
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -23,22 +23,41 @@ class ClassicGameFragment : Fragment() {
 
         val gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        val tv1 = view.findViewById<TextView>(R.id.fragment_classicGame_tv_1)
+
+        val textArray = Array<Array<TextView>>(4){Array<TextView>(4){it -> view.findViewById<TextView>(R.id.fragment_classicGame_tv_00)} }
+        textArray[0][0] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_00)
+        textArray[0][1] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_01)
+        textArray[0][2] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_02)
+        textArray[0][3] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_03)
+        textArray[1][0] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_10)
+        textArray[1][1] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_11)
+        textArray[1][2] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_12)
+        textArray[1][3] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_13)
+        textArray[2][0] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_20)
+        textArray[2][1] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_21)
+        textArray[2][2] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_22)
+        textArray[2][3] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_23)
+        textArray[3][0] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_30)
+        textArray[3][1] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_31)
+        textArray[3][2] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_32)
+        textArray[3][3] = view.findViewById<TextView>(R.id.fragment_classicGame_tv_33)
 
 
 
-        ObjectAnimator.ofFloat(tv1,"translationX",100f).apply {
-            duration = 2000
-            start()
-            GlobalScope.launch {
-                delay(2000)
-                tv1.setText("4")
-            }
-        }
-        GlobalScope.launch {
-            delay(3000)
-            goToFragment(ClassicGameFragment())
-        }
+
+//        animation
+//        ObjectAnimator.ofFloat(tv1,"translationX",100f).apply {
+//            duration = 1000
+//            start()
+//            GlobalScope.launch {
+//                delay(1000)
+//                tv1.setText("4")
+//            }
+//        }
+//        GlobalScope.launch {
+//            delay(3000)
+//            goToFragment(ClassicGameFragment())
+//        }
 
 
         return view
@@ -46,6 +65,8 @@ class ClassicGameFragment : Fragment() {
 
 
     private fun goToFragment(fragment: Fragment) {
+
+
         requireActivity().supportFragmentManager
             .beginTransaction()
             .replace(R.id.mainActivity_fcv_default,fragment)
--- app/src/main/java/com/example/game2048/GameViewModel.kt
+++ app/src/main/java/com/example/game2048/GameViewModel.kt
@@ -1,7 +1,53 @@
 package com.example.game2048
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+    val numArray = Array(4) { Array(4) { it -> 0 } }
+
+
+    //Initialize the game, randomly select three positions to initialize to 2, the positions can be repeated
+    fun gameInit(){
+        var randomNum : Int
+        var row : Int
+        var column : Int
+        for(row in 0..3){
+            for(column in 0..3){
+                numArray[row][column] = 0
+            }
+        }
+        repeat(3){
+            randomNum = (0..15).random()
+            row = randomNum/4
+            column = randomNum%4
+            numArray[row][column] = 2
+        }
+    }
+
+    //Add a new random number every time the user survive
+    fun generateNum(){
+        var row : Int
+        var column : Int
+        val emptyPos = mutableListOf<Int>()
+        for(row in 0..3){
+            for(column in 0..3){
+                if(numArray[row][column] == 0){
+                    emptyPos.add(row*4+column)
+                }
+            }
+        }
+        val randomNum = (0 until emptyPos.size).random()
+        row = randomNum/4
+        column = randomNum%4
+
+    }
+
+    fun swipeLeft(){
+
+    }
+
+
+
 
 }
